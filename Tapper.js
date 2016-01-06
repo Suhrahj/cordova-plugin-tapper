89917c49146811c1ed6445839872f63e38b7fc62
Suhrahj <denizcanbilgin@gmail.com>
2016-01-06 04:31:38 +0200

    Update Tapper.js

--- Tapper.js
+++ Tapper.js
@@ -3,19 +3,4 @@ var self = module.exports = {
 	acoustic: function() {
 		cordova.exec(function() {}, function() {}, 'Tapper', 'Sound', [])
 	},
-
-	isFeedbackEnabled: function(success) {
-		// verify, that argument is function
-		if(typeof success != "function") {
-			throw "Argument must be callback function."
-		}
-	
-		cordova.exec(
-			success,
-			function() {}, 
-			'DeviceFeedback', 
-			'isFeedbackEnabled', 
-			[]
-		)
-	}
 }
