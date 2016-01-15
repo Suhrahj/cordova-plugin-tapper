65fcb6be81bcc06afc8a95a9dd0034abb57519f7
Suhrahj <denizcanbilgin@gmail.com>
2016-01-15 23:38:11 +0200

    Update Tapper.m

--- src/ios/Tapper.m
+++ src/ios/Tapper.m
@@ -5,6 +5,14 @@
 - (void)Sound:(CDVInvokedUrlCommand*)command
 {
 
+NSString* callbackId = [command callbackId];
+    NSString* name = [[command arguments] objectAtIndex:0];
+    NSString* msg = [NSString stringWithFormat: @"Hello, %@", name];
+
+    CDVPluginResult* result = [CDVPluginResult
+                               resultWithStatus:CDVCommandStatus_OK
+                               messageAsString:msg];
+                               
     AudioServicesPlaySystemSound(1104);
 
     [self success:result callbackId:callbackId];
