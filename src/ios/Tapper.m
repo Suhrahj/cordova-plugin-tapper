300f32f4c57cd99ef97141d877934dd53a85d872
Suhrahj <denizcanbilgin@gmail.com>
2016-02-07 22:51:14 +0200

    Update Tapper.m

--- src/ios/Tapper.m
+++ src/ios/Tapper.m
@@ -15,7 +15,7 @@
                                resultWithStatus:CDVCommandStatus_OK
                                messageAsString:msg];
 
-    [self success:result callbackId:callbackId];
+    //[self success:result callbackId:callbackId];
 }
 
 @end
